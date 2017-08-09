53ed68defe7a56d6a537a8a222c5a1a411c1c8f5
M Manzur Morshed <mmorshed@asu.edu>
2017-08-08 20:09:12 -0400

    Update largest_number.py

--- largest_number.py
+++ largest_number.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python
+#Given a list of non negative integers, arrange them such that they form the largest number.
+#
+#For example:
+#
+#Given [3, 30, 34, 5, 9], the largest formed number is 9534330.
+#
+#Note: The result may be very large, so you need to return a string instead of an integer.
 def large_seq(a0,a1):
      b0 = str(a0)
      b1 = str(a1)
